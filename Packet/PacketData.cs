95fc879379c5bf83c7ad0483a89bd2a5c7af267e
Thejuster <realquaker@hotmail.it>
2022-11-21 19:49:44 +0100

    Reorder
    Reorder all Folder

---
+++ Packet/PacketData.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+//------------------------
+// Packet System by Thejuster
+// MMORpgmaker Packet System
+// (C) Copyright MakingItalia
+//------------------------
+
+namespace Packet
+{
+
+    public class PacketBase
+    {
+
+        public object PaketType(byte[] value)
+        {
+            byte[] data = new byte[100];
+            object t = null;
+
+            PacketData pdata = new PacketData();
+            CharPaket cdata = new CharPaket();
+
+            return t;
+        }
+
+    }
+
+
+    /// <summary>
+    /// Generic Packet Data  [ 0x0 ]
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct PacketData
+    {
+        public uint Command;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 24)]
+        public String Argument1;
+
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 24)]
+        public String Argument2;
+
+        public byte clienttype;
+
+
+        public byte[] Serialize()
+        {
+
+            var buffer = new byte[Marshal.SizeOf(typeof(PacketData))];
+
+            var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+            var pBuffer = gch.AddrOfPinnedObject();
+
+            Marshal.StructureToPtr(this, pBuffer, false);
+            gch.Free();
+
+            return buffer;
+        }
+
+
+        public void Deserialize(ref byte[] data)
+        {
+            var gch = GCHandle.Alloc(data, GCHandleType.Pinned);
+            this = (PacketData)Marshal.PtrToStructure(gch.AddrOfPinnedObject(), typeof(PacketData));
+            gch.Free();
+        }
+
+
+    }
+
+
+    /// <summary>
+    /// Charaset Packet System  (Note:  60 Byte Totali ) [ 0x1 ]
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct CharPaket
+    {
+        public uint Command;
+
+        public int accountID;
+
+        public int CharNum;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 24)]
+        public string CharName;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 24)]
+        public string Class;
+
+        public int level;
+
+        public int exp;
+
+        public int hp;
+        public int sp;
+        public int max_hp;
+        public int max_sp;
+        public int str;
+        public int agi;
+        public int vit;
+        public int intel;
+        public int dex;
+        public int luk;
+        public int hair_id;
+
+        public byte[] Serialize()
+        {
+
+            var buffer = new byte[Marshal.SizeOf(typeof(CharPaket))];
+
+            var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+            var pBuffer = gch.AddrOfPinnedObject();
+
+            Marshal.StructureToPtr(this, pBuffer, false);
+            gch.Free();
+
+            return buffer;
+        }
+
+        public void Deserialize(ref byte[] data)
+        {
+            var gch = GCHandle.Alloc(data, GCHandleType.Pinned);
+            this = (CharPaket)Marshal.PtrToStructure(gch.AddrOfPinnedObject(), typeof(CharPaket));
+            gch.Free();
+        }
+    }
+
+
+
+
+    public class PacketHeader
+    {
+        public enum HeaderCommand : uint
+        {
+            ACT_LOGIN = 0x41,
+            ACT_MSG = 0x15,
+            ACT_GET_P2LOC = 0x23,
+            ACT_REQ_ROOM = 0x44,
+            ACT_GET_ROOM = 0x45,
+            ACT_CHAR_DEATH = 0x40,
+            ACT_GET_ACC_ID = 0x16,  //Get Account ID
+            ACT_REC_ACC_ID = 0x17, //Riceve le informazioni
+            ACT_GET_CHAR = 0x18,
+            CHAR_EMPTY = 0x19,
+            ACT_COUNT_CHAR = 0x20,
+            ACT_GET_CHARCOUNT = 0x21,
+            ACT_CREATE_CHAR = 0x22,
+            ACT_CONFIRM = 0x50,
+            ACT_FAIL = 0x51
+
+        }
+
+
+        public enum PacketType : byte
+        {
+            PacketData = 0,
+            CharPacket = 1
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,76 @@
+﻿using MySql.Data.MySqlClient;
+using System.Net.Sockets;
+
+MMORpgmakerServer.Server s = new MMORpgmakerServer.Server();
+s.Main();
+
+
+
+namespace MMORpgmakerServer
+{
+
+    public class Server
+    {
+        public TcpListener tcpListener;
+        public Thread listenThread;
+
+
+        string host_ip;
+        int host_port = 6500;
+        string mysql_host = "";
+        int mysql_host_port = 3306;
+        string mysql_user = "";
+        string mysql_pass = "";
+        string mysql_db = "";
+        ConsoleColor Fore = ConsoleColor.Red;
+        ConsoleColor Back = ConsoleColor.Black;
+
+        MySqlConnection cn;
+        const int PacketSize = 120;
+
+
+
+
+
+        public void Main()
+        {
+            //connessione = "server=" + host + ";uid=" + user + ";pwd=" + psw + ";database=" + db;
+            Console.WriteLine("Hello World");
+           
+            Console.ReadKey();
+        }
+
+
+
+        public static void Crediti()
+        {
+            ConsoleColor c = Console.ForegroundColor;
+            ConsoleColor bg = Console.BackgroundColor;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine(@"                                                                                            ");
+            Console.WriteLine(@"   _____      _____   ________ __________                               __                  ");
+            Console.WriteLine(@"  /     \    /     \  \_____  \\______   \______   ____   _____ _____  |  | __ ___________  ");
+            Console.WriteLine(@" /  \ /  \  /  \ /  \  /   |   \|       _/\____ \ / ___\ /     \\__  \ |  |/ // __ \_  __ \ ");
+            Console.WriteLine(@"/    Y    \/    Y    \/    |    \    |   \|  |_> > /_/  >  Y Y  \/ __ \|    <\  ___/|  | \/ ");
+            Console.WriteLine(@"\____|__  /\____|__  /\_______  /____|_  /|   __/\___  /|__|_|  (____  /__|_ \\___  >__|    ");
+            Console.WriteLine(@"        \/         \/         \/       \/ |__|  /_____/       \/     \/     \/    \/        ");
+            Console.WriteLine(@"                                                                                            ");
+            Console.WriteLine(@" By Thejuster - (C) Copyright Making Italia                                                 ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine(@$" Core Server  - Hostin to {host_ip} : {host_port}");
+            Console.WriteLine("");
+
+            Console.ForegroundColor = c;
+            Console.BackgroundColor = bg;
+
+        }
+
+
+
+    }
+
+}
+
